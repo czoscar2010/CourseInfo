1309d2c8ace0a0339da27c7e2f0825ddbca275cc
czoscar2010 <czoscar2010@hotmail.com>
2018-12-28 19:33:47 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t1;
+import java.sql.*;
+/**
+/**
+ *
+ * @author czosc
+ */
+
+
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) {
+        
+       show a = new show();
+       a.show();
+        // TODO code application logic here
+    }
+    
+}
